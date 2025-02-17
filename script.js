e5a76d00f4bd5737c4554e8e66966bcc3f0aad04
WAHIDUL <wahidlhassan60@gmail.com>
2025-02-17 18:11:39 +0600

    last Update

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log(StyleSheet)
+let cardText=document.querySelector(".card-text")
+let btn=document.querySelector(".btn")
+let cardTittle=document.querySelector(".card-title")
+let card=document.querySelectorAll(".card")
+console.log(card)
+
+let arr=[]
+
+card.innerHTML="Wahidul"
+
+btn.addEventListener("click",function(){
+    cardTittle.innerHTML=`<h1> ${arr[0].taskName}</h1>`
+})
+
+
+
